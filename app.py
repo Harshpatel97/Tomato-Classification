851799af0b9c7330ed3be656661209e835eea5e9
Harsh Patel <harshp4106@gmail.com>
2023-09-20 11:36:47 +0530

    updated app.py

--- app.py
+++ app.py
@@ -1,17 +1,7 @@
-import torchvision
-from torch import nn
 from pathlib import Path
-import time
-import torch, torchvision
-
 import torch
 import random
-import matplotlib.pyplot as plt
-from typing import Tuple, List, Dict
-from torch import nn
-
-from PIL import Image
-from torch.utils.data import Dataset
+from typing import Tuple, Dict
 from torchvision import transforms
 
 from timeit import default_timer as timer 
@@ -19,34 +9,30 @@ import gradio as gr
 
 loc = Path("Data\PlantVillage")
 
-data_transform = transforms.Compose([
-    transforms.Resize(size=(224,224)),
-    transforms.ToTensor(),
-    #transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-])
-
-class_names = ['Bacterial_spot',
-                 'Curl_Virus',
-                 'Early_blight',
-                 'Healthy',
-                 'Late_blight',
-                 'Leaf_Mold',
-                 'Mosaic_virus',
-                 'Septoria_leaf_spot',
-                 'Target_Spot',
-                 'Two_spotted_spider_mite']
-
-model = torchvision.models.efficientnet_b2().cpu
-
-## Provide the model.pth path from your models folder..
-saved_model = torch.load(r"models\model.pth",
-                         map_location=torch.device('cpu') ## If you dont have gpu you have to write this fucntion
-                         )
-
-
 def predict(img) -> Tuple[Dict, float]:
     """Transforms and performs a prediction on img and returns prediction and time taken.
     """
+    data_transform = transforms.Compose([
+        transforms.Resize(size=(224,224)),
+        transforms.ToTensor(),
+        #transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    ])
+
+    class_names = ['Bacterial_spot',
+                    'Curl_Virus',
+                    'Early_blight',
+                    'Healthy',
+                    'Late_blight',
+                    'Leaf_Mold',
+                    'Mosaic_virus',
+                    'Septoria_leaf_spot',
+                    'Target_Spot',
+                    'Two_spotted_spider_mite']
+
+    ## Provide the model.pth path from your models folder..
+    saved_model = torch.load("models\model.pth",
+                            map_location=torch.device('cpu') ## If you dont have gpu you have to write this fucntion
+                            )
     # Start the timer
     start_time = timer()
     
@@ -80,7 +66,7 @@ description = "An EfficientNetB2 feature extractor computer vision model to clas
 demo = gr.Interface(fn=predict, # mapping function from input to output
                     inputs=gr.Image(type="pil"), # what are the inputs?
                     outputs=[gr.Label(num_top_classes=3, label="Predictions"), # what are the outputs?
-                             gr.Number(label="Prediction time (s)")], # our fn has two outputs, therefore we have two outputs 
+                            gr.Number(label="Prediction time (s)")], # our fn has two outputs, therefore we have two outputs 
                     title=title,
                     examples=example_list,
                     description=description)
